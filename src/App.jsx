d67cf49115e41f40c7f84b929c80b66eb72526ac
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-28 10:20:53 +0530

    created login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 
 export default function App() {
-  return <SignUp />;
+  const [isAuth, setIsAuth] = useState(false);
+
+  function login() {
+    setIsAuth(true);
+  }
+
+  return <>{isAuth ? <Home /> : <SignUp login={login} />}</>;
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./CSS/SignUp.css";
 import env from "../env";
 
-export default function SignUp() {
+export default function SignUp({ login }) {
+  const [isSignup, setIsSignup] = useState(true);
+
+  function toggleSignup() {
+    setIsSignup((isSignup) => !isSignup);
+  }
+
   const signupEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${env.apiKey}`;
+  const loginEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${env.apiKey}`;
 
   const email = useRef();
   const password = useRef();
@@ -11,50 +18,77 @@ export default function SignUp() {
 
   async function submitHandler(e) {
     e.preventDefault();
-    if (password.current.value !== confirmedPassword.current.value) {
-      return;
-    }
-    console.log("a");
-    const res = await fetch(signupEndpoint, {
-      method: "POST",
-      body: JSON.stringify({
-        email: email.current.value,
-        password: password.current.value,
-        returnSecureToken: true,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log("b");
-    if (res.ok) {
-      console.log("c");
-      const data = await res.json();
-      console.log("User has successfully signed up");
-    } else {
-      console.log("d");
-      const data = await res.json();
-      let errorMessage = "Authentication Failed";
-      if (data.error.message) {
-        errorMessage = data.error.message;
+    if (isSignup) {
+      if (password.current.value !== confirmedPassword.current.value) {
+        return;
       }
-      alert(errorMessage);
+      const res = await fetch(signupEndpoint, {
+        method: "POST",
+        body: JSON.stringify({
+          email: email.current.value,
+          password: password.current.value,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log("User has successfully signed up");
+      } else {
+        const data = await res.json();
+        let errorMessage = "Authentication Failed";
+        if (data.error.message) {
+          errorMessage = data.error.message;
+        }
+        alert(errorMessage);
+      }
+    } else {
+      const res = await fetch(loginEndpoint, {
+        method: "POST",
+        body: JSON.stringify({
+          email: email.current.value,
+          password: password.current.value,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        localStorage.setItem("token", data.idToken);
+        login();
+      } else {
+        const data = await res.json();
+        let errorMessage = "Authentication Failed";
+        if (data.error.message) {
+          errorMessage = data.error.message;
+        }
+        alert(errorMessage);
+      }
     }
   }
 
   return (
     <div>
-      <h3>Sign Up</h3>
+      <h3>{isSignup ? "Sign Up" : "Login"}</h3>
       <form onSubmit={submitHandler}>
         <input type="email" placeholder="Email" ref={email} />
         <input type="password" placeholder="Password" ref={password} />
-        <input
-          type="password"
-          placeholder="Confirmed Password"
-          ref={confirmedPassword}
-        />
-        <input type="submit" value="Sign Up" />
+        {isSignup && (
+          <input
+            type="password"
+            placeholder="Confirmed Password"
+            ref={confirmedPassword}
+          />
+        )}
+        <input type="submit" value={isSignup ? "Sign Up" : "Login"} />
       </form>
+      <button onClick={toggleSignup}>
+        Or {isSignup ? "Login" : "Sign Up"}
+      </button>
     </div>
   );
 }
